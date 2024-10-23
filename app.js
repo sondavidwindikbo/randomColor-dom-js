6a4d8c87aff397a84ef7e7a2d399024cffaebdd2
unknown <windikboson@gmail.com>
2024-10-23 20:35:59 +0900

    belajar event element

---
+++ app.js
@@ -0,0 +1,41 @@
+// const button = document.querySelector('button');
+
+// const h1 = document.querySelector('h1');
+
+
+// button.addEventListener('click', () => {
+//     const newColor = generateRandomColor();
+//     document.body.style.backgroundColor = newColor;
+//     h1.innerText = newColor;
+//     console.log(newColor);
+// })
+
+
+// const generateRandomColor = () => {
+//     const r = Math.floor(Math.random() * 255);
+//     const g = Math.floor(Math.random() * 255);
+//     const b = Math.floor(Math.random() * 255);
+
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+
+button.addEventListener('click', ()=> {
+    const newColor = generateRandomColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+    console.log(newColor);
+})
+
+
+const generateRandomColor = () =>{
+    const r = Math.floor(Math.random() * 225);
+    const g = Math.floor(Math.random() * 225);
+    const b = Math.floor(Math.random() * 225);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
